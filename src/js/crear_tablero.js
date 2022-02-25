d19fab1076548ba524bca285804489e5c1362981
acorbacho <aaroncorbachovieites@gmail.com>
2022-02-25 21:12:37 +0100

    beta js code

---
+++ src/js/crear_tablero.js
@@ -0,0 +1,12 @@
+function crear_tablero(tablero) {
+    var tableroAux = tablero
+    var mensaje = ''
+
+    for (var i = 0; i < tableroAux.length; i++) {
+        for (var j = 0; j < tableroAux[i].length; j++) {
+            mensaje = mensaje + tableroAux[i][j] + ' '
+        }
+        mensaje = mensaje + "\n"
+    }
+    return mensaje
+}
