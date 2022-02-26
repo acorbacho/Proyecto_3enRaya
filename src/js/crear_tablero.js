b2eac63933db367bdb15becb6d5da2d5c1cd36d1
acorbacho <aaroncorbachovieites@gmail.com>
2022-02-26 20:55:23 +0100

    etc

--- src/js/crear_tablero.js
+++ src/js/crear_tablero.js
@@ -5,6 +5,7 @@ function crear_tablero(tablero) {
     for (var i = 0; i < tableroAux.length; i++) {
         for (var j = 0; j < tableroAux[i].length; j++) {
             mensaje = mensaje + tableroAux[i][j] + ' '
+            document.getElementById("f" + i + "c" + j).innerHTML = tableroAux[i][j]
         }
         mensaje = mensaje + "\n"
     }
