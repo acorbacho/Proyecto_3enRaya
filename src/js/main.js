6f0aa88f2e31c7461756fd695b015954cfbc4c09
acorbacho <aaroncorbachovieites@gmail.com>
2022-03-11 17:24:46 +0100

    Squashed commit of the following:
    commit 4015a8487f20935c2f681389cd64d1ef179d6bbc
    Author: acorbacho <aaroncorbachovieites@gmail.com>
    Date:   Mon Mar 7 07:24:17 2022 +0100
    
        Mejoras HTML
    
    commit 2fc5637bb3a8057ad2ccf263b026f4f75ceff174
    Author: acorbacho <aaroncorbachovieites@gmail.com>
    Date:   Mon Mar 7 07:24:07 2022 +0100
    
        Mejoras estilo
    
    commit 5ffc68c1c4234cbadfe7d613433593b044c4f07d
    Author: acorbacho <aaroncorbachovieites@gmail.com>
    Date:   Mon Mar 7 07:23:59 2022 +0100
    
        Mejoras JS
    
    commit 7905b8df67f29ddd58c4f113589111df9b740b93
    Author: acorbacho <aaroncorbachovieites@gmail.com>
    Date:   Mon Mar 7 06:56:34 2022 +0100
    
        Imágenes SVG
    
    commit 40f6c9b5eae2e7f1aba451f94bc6f32bf110a208
    Author: acorbacho <aaroncorbachovieites@gmail.com>
    Date:   Mon Mar 7 06:56:17 2022 +0100
    
        Eliminación archivo

--- src/js/main.js
+++ src/js/main.js
@@ -1,51 +1,59 @@
 function main() {
-    var isRunning = true
-    var continuar = ''
-    var tablero = [['7', '8', '9'], ['4', '5', '6'], ['1', '2', '3']]
-    var tablero_mensaje = crear_tablero(tablero)
-    var posicion = ''
-    var x_o = ''
-    var contador = 0
-    salida('Bienvenido al 3 en raya.')
 
-    while (isRunning == true) {
-        x_o = entrada('Introduce la marca "x" o "o" que deseas introducir.')
-        contador = 0
-
-        if (x_o != 'o' && x_o != 'x') {
-            salida('Marca no válida')
-        } else {
-            posicion = entrada('Introduce el número del tablero donde quieres poner la marca.\n' + tablero_mensaje)
-
-            for (var i = 0; i < tablero.length; i++) {
-
-                for (var j = 0; j < tablero[i].length; j++) {
-
-                    if (tablero[i][j] == posicion) {
-                        tablero[i][j] = x_o
-                        contador++
-                    }
-                }
-            }
-            contador++
-        }
-
-        if (contador == 0) {
-            continuar = entrada('¿Continuar?\n Pulse "Aceptar" para continuar.\n Escriba "no" y pulse "Aceptar" para salir.')
-        } else if (contador == 1) {
-            salida('Posición no válida.')
-            continuar = entrada('¿Continuar?\n Pulse "Aceptar" para continuar.\n Escriba "no" y pulse "Aceptar" para salir.')
-        } else if (contador == 2) {
-            tablero_mensaje = crear_tablero(tablero)
-            salida(tablero_mensaje)
-            continuar = entrada('¿Continuar?\n Pulse "Aceptar" para continuar.\n Escriba "no" y pulse "Aceptar" para salir.')
-        }
-
-        if (continuar == 'no' || continuar == 'No' || continuar == 'NO' || continuar == 'N' || continuar == 'n') {
-            isRunning = !isRunning
-        } else {
-            isRunning = isRunning
-        }
+  //Variables.
+  let contador_casillas = 0
+  let casillas = []
+  let jugador = true
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      casillas[contador_casillas] = document.getElementById("f" + i + "c" + j)
+      contador_casillas++
     }
+  }
+
+  //Eventos.
+  for (let i = 0; i < 9; i++) {
+    casillas[i].addEventListener('click', () => {
+      movimiento(casillas[i])
+    })
+  }
+
+  //Funciones.
+  function movimiento(casilla) {
+    if (jugador) {
+      casilla.innerHTML = '<img src="../assets/img/nature_space_sun.svg">'
+    } else {
+      casilla.innerHTML = '<img src="../assets/img/nature_space_moon.svg">'
+    }
+    jugador = !jugador
+    if (casillas[0].innerHTML === casillas[1].innerHTML && casillas[0].innerHTML === casillas[2].innerHTML && casillas[0].innerHTML != null) {
+      salida('Win1')
+    } else if (casillas[3].innerHTML === casillas[4].innerHTML && casillas[3].innerHTML === casillas[5].innerHTML && casillas[3].innerHTML != '') {
+      salida('Win2')
+    } else if (casillas[6].innerHTML === casillas[7].innerHTML && casillas[6].innerHTML === casillas[8].innerHTML && casillas[6].innerHTML != '') {
+      salida('Win3')
+    } else if (casillas[0].innerHTML === casillas[3].innerHTML && casillas[0].innerHTML === casillas[6].innerHTML && casillas[0].innerHTML != '') {
+      salida('Win4')
+    } else if (casillas[1].innerHTML === casillas[4].innerHTML && casillas[1].innerHTML === casillas[7].innerHTML && casillas[1].innerHTML != '') {
+      salida('Win5')
+    } else if (casillas[2].innerHTML === casillas[5].innerHTML && casillas[2].innerHTML === casillas[8].innerHTML && casillas[2].innerHTML != '') {
+      salida('Win6')
+    } else if (casillas[0].innerHTML === casillas[4].innerHTML && casillas[0].innerHTML === casillas[8].innerHTML && casillas[0].innerHTML != '') {
+      salida('Win7')
+    } else if (casillas[2].innerHTML === casillas[4].innerHTML && casillas[2].innerHTML === casillas[6].innerHTML && casillas[2].innerHTML != '') {
+      salida('Win8')
+    } else {
+      contador_casillas = 0
+      for (let i = 0; i < casillas.length; i++) {
+        if (casillas[i].innerHTML != '') {
+          contador_casillas++
+        }
+      }
+      if (contador_casillas === 9) {
+        salida('Empate')
+        location.reload()
+      }
+    }
+  }
 }
 main()
