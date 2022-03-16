bb30119c347d5cc99fe13d9aa665589e6df785e5
Rubén <rmartinezdominguez@danielcastelao.org>
2022-03-16 19:00:37 +0100

    implementado recuento de victorias

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,26 @@
 function main() {
-
     //Variables.
     let contador_casillas = 0
     let casillas = []
     let jugador = true
     let var_win = 30000
     let banner_victoria = document.getElementById("banner_victoria")
+    var victorias_J1 = 0
+    let vicJ1_HTML = document.getElementById("J1VIC")
+    var victorias_J2 = 0
+    let vicJ2_HTML = document.getElementById("J2VIC")
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
             casillas[contador_casillas] = document.getElementById("f" + i + "c" + j)
             contador_casillas++
         }
     }
-
     //Eventos.
     for (let i = 0; i < 9; i++) {
         casillas[i].addEventListener('click', () => {
             movimiento(casillas[i])
         })
     }
-
     //Funciones.
     function movimiento(casilla) {
         if (jugador) {
@@ -28,35 +29,27 @@ function main() {
             casilla.innerHTML = '<img src="../assets/img/nature_space_moon.svg">'
         }
         if (casillas[0].innerHTML === casillas[1].innerHTML && casillas[0].innerHTML === casillas[2].innerHTML && casillas[0].innerHTML != '') {
-            salida('Win1');
             var_win = 1;
             animaWin(var_win, jugador);
         } else if (casillas[3].innerHTML === casillas[4].innerHTML && casillas[3].innerHTML === casillas[5].innerHTML && casillas[3].innerHTML != '') {
-            salida('Win2');
             var_win = 2;
             animaWin(var_win, jugador);
         } else if (casillas[6].innerHTML === casillas[7].innerHTML && casillas[6].innerHTML === casillas[8].innerHTML && casillas[6].innerHTML != '') {
-            salida('Win3');
             var_win = 3;
             animaWin(var_win, jugador);
         } else if (casillas[0].innerHTML === casillas[3].innerHTML && casillas[0].innerHTML === casillas[6].innerHTML && casillas[0].innerHTML != '') {
-            salida('Win4');
             var_win = 4;
             animaWin(var_win, jugador);
         } else if (casillas[1].innerHTML === casillas[4].innerHTML && casillas[1].innerHTML === casillas[7].innerHTML && casillas[1].innerHTML != '') {
-            salida('Win5');
             var_win = 5;
             animaWin(var_win, jugador);
         } else if (casillas[2].innerHTML === casillas[5].innerHTML && casillas[2].innerHTML === casillas[8].innerHTML && casillas[2].innerHTML != '') {
-            salida('Win6');
             var_win = 6;
             animaWin(var_win, jugador);
         } else if (casillas[0].innerHTML === casillas[4].innerHTML && casillas[0].innerHTML === casillas[8].innerHTML && casillas[0].innerHTML != '') {
-            salida('Win7');
             var_win = 7;
             animaWin(var_win, jugador);
         } else if (casillas[2].innerHTML === casillas[4].innerHTML && casillas[2].innerHTML === casillas[6].innerHTML && casillas[2].innerHTML != '') {
-            salida('Win8');
             var_win = 8;
             animaWin(var_win, jugador);
         } else {
@@ -79,10 +72,13 @@ function main() {
         if (jugador) {
             estrella_win = '<img src="../assets/img/star1.svg">';
             texto_victoria = '<svg width="auto"height="100"><circle cx="100"cy="50"r="50"fill="#ffd900"viewBox="0 0 100 100"><animate attributeType="XML"attributeName="cx"from="-1490"to="2320"dur="5s"begin="0s"repeatCount="indefinite"/></circle><text x="10"y="70"style="font:bold 4.2em arial;fill:white;"viewBox="0 0 100 100">J1<animate attributeType="XML"attributeName="x"from="-1525"to="2285"dur="5s"begin="0s"repeatCount="indefinite"/></text><text x="0"y="70"style="font:bold 4.2em arial;fill:black;"viewBox="0 0 100 100">El ganador es<animate attributeType="XML"attributeName="x"from="-2030"to="1800"dur="5s"begin="0s"repeatCount="indefinite"/></text></svg>';
-
+            victorias_J1++
+            vicJ1_HTML.innerHTML = victorias_J1
         } else {
             estrella_win = '<img src="../assets/img/star2.svg">';
             texto_victoria = '<svg width="auto"height="100"><circle cx="100"cy="50"r="50"fill="Blue"viewBox="0 0 100 100"><animate attributeType="XML"attributeName="cx"from="-1490"to="2320"dur="5s"begin="0s"repeatCount="indefinite"/></circle><text x="10"y="70"style="font:bold 4.2em arial;fill:white;"viewBox="0 0 100 100">J2<animate attributeType="XML"attributeName="x"from="-1525"to="2285"dur="5s"begin="0s"repeatCount="indefinite"/></text><text x="0"y="70"style="font:bold 4.2em arial;fill:black;"viewBox="0 0 100 100">El ganador es<animate attributeType="XML"attributeName="x"from="-2030"to="1800"dur="5s"begin="0s"repeatCount="indefinite"/></text></svg>';
+            victorias_J2++
+            vicJ2_HTML.innerHTML = victorias_J2
         }
         switch (var_win) {
             case 1:
@@ -118,6 +114,5 @@ function main() {
         casillas[casilla3].innerHTML = estrella_win
         banner_victoria.innerHTML = texto_victoria
     }
-
 }
 main()
